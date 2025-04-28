ffcb11723975c68a64d90badea798fd22c2859c5
9428260 <9428260@gmail.com>
2025-04-28 13:48:18 +0900

    Update n8n.py

--- n8n.py
+++ n8n.py
@@ -8,7 +8,7 @@ import requests, json, warnings
 class Pipeline:
     def __init__(self):
         self.name = "N8N Agent Pipeline"
-        self.api_url = "https://n8n.skcc-infra.net/webhook/myflow"     # Set correct hostname
+        self.api_url = "https://n8n.skcc-infra.net/webhook-test/myflow"     # Set correct hostname
         self.api_key = ""                                    # Insert your actual API key here
         self.verify_ssl = False
         self.debug = False
